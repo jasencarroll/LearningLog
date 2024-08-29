c6421018dbe879269e2d59e20b1add16ba5b4402
Jasen Carroll <108644550+jasencarroll@users.noreply.github.com>
2024-08-29 16:38:24 -0400

    not posting, but bootstrapped

--- ll_project/settings.py
+++ ll_project/settings.py
@@ -33,6 +33,10 @@ ALLOWED_HOSTS = []
 INSTALLED_APPS = [
     # My apps.
     'learning_logs',
+    'accounts',
+
+    # Third part apps.
+    'django_bootstrap5',
     
     # Default django apps
     'django.contrib.admin',
@@ -125,3 +129,8 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# My settings.
+LOGIN_REDIRECT_URL = 'learning_logs:index'
+LOGOUT_REDIRECT_URL = 'learning_logs:index'
+LOGIN_URL = 'accounts:login'
